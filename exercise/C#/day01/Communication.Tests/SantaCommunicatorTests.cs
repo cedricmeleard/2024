6f6d3a525c252897a1aaa5280e9a707685c5e6a9
cedric <cedric.meleard@gmail.com>
2024-12-01 13:30:37 +0100

    Day01 Add ValueObject for Location & Reindeer name

--- exercise/C#/day01/Communication.Tests/SantaCommunicatorTests.cs
+++ exercise/C#/day01/Communication.Tests/SantaCommunicatorTests.cs
@@ -1,47 +1,48 @@
+using Communication.Domain;
 using Communication.Tests.Doubles;
 using FluentAssertions;
 using Xunit;
 
-namespace Communication.Tests
+namespace Communication.Tests;
+
+public class SantaCommunicatorTests
 {
-    public class SantaCommunicatorTests
+    private const string Dasher = "Dasher";
+    private const string NorthPole = "North Pole";
+    private const int NumberOfDaysToRest = 2;
+    private const int NumberOfDayBeforeChristmas = 24;
+    private readonly TestLogger _logger = new();
+    private readonly SantaCommunicator _communicator = new(NumberOfDaysToRest);
+
+    [Fact]
+    public void ComposeMessage()
+        => _communicator.ComposeMessage(
+                new ReindeerName(Dasher), 
+                new Location(NorthPole, 5), 
+                NumberOfDayBeforeChristmas)
+            .Should()
+            .Be("Dear Dasher, please return from North Pole in 17 day(s) to be ready and rest before Christmas.");
+
+    [Fact]
+    public void ShouldDetectOverdueReindeer()
     {
-        private const string Dasher = "Dasher";
-        private const string NorthPole = "North Pole";
-        private const int NumberOfDaysToRest = 2;
-        private const int NumberOfDayBeforeChristmas = 24;
-        private readonly TestLogger _logger = new();
-        private readonly SantaCommunicator _communicator = new(NumberOfDaysToRest);
+        var overdue = _communicator.IsOverdue(
+            new ReindeerName(Dasher), 
+            new Location(NorthPole, NumberOfDayBeforeChristmas), 
+            NumberOfDayBeforeChristmas, 
+            _logger);
 
-        [Fact]
-        public void ComposeMessage()
-            => _communicator.ComposeMessage(Dasher, NorthPole, 5, NumberOfDayBeforeChristmas)
-                .Should()
-                .Be("Dear Dasher, please return from North Pole in 17 day(s) to be ready and rest before Christmas.");
-
-        [Fact]
-        public void ShouldDetectOverdueReindeer()
-        {
-            var overdue = _communicator.IsOverdue(
-                Dasher,
-                NorthPole,
-                NumberOfDayBeforeChristmas,
-                NumberOfDayBeforeChristmas,
-                _logger);
-
-            overdue.Should().BeTrue();
-            _logger.LoggedMessage().Should().Be("Overdue for Dasher located North Pole.");
-        }
-
-        [Fact]
-        public void ShouldReturnFalseWhenNoOverdue()
-            => _communicator.IsOverdue(
-                    Dasher,
-                    NorthPole,
-                    NumberOfDayBeforeChristmas - NumberOfDaysToRest - 1,
-                    NumberOfDayBeforeChristmas,
-                    _logger)
-                .Should()
-                .BeFalse();
+        overdue.Should().BeTrue();
+        _logger.LoggedMessage().Should().Be("Overdue for Dasher located North Pole.");
     }
+
+    [Fact]
+    public void ShouldReturnFalseWhenNoOverdue()
+        => _communicator.IsOverdue(
+                new ReindeerName(Dasher), 
+                new Location(NorthPole, NumberOfDayBeforeChristmas - NumberOfDaysToRest - 1), 
+                NumberOfDayBeforeChristmas, 
+                _logger)
+            .Should()
+            .BeFalse();
 }
