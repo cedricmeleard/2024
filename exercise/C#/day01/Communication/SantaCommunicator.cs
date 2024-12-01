ab76e975d52a989828c062bbf3eda071742a3b59
cedric <cedric.meleard@gmail.com>
2024-12-01 18:22:41 +0100

    Refactor: Introduce DaysForReturn for calculating reindeer return
    Add constant for messages, allow a single place to regroup and change them if needed
    
    Also, ComposeMessage Transformed to expression body form but not sure with it, if it's readable enough

--- exercise/C#/day01/Communication/SantaCommunicator.cs
+++ exercise/C#/day01/Communication/SantaCommunicator.cs
@@ -4,29 +4,23 @@ namespace Communication;
 
 public class SantaCommunicator(int numberOfDaysToRest)
 {
-    public string ComposeMessage(
-        Reindeer reindeer,
-        int numberOfDaysBeforeChristmas)
-    {
-        var daysBeforeReturn = DaysBeforeReturn(reindeer.NumbersOfDaysForComingBack, numberOfDaysBeforeChristmas);
-        return
-            $"Dear {reindeer.Name}, please return from {reindeer.CurrentLocation} in {daysBeforeReturn} day(s) to be ready and rest before Christmas.";
-    }
+    private const string MessageForReindeerToReturn = "Dear {0}, please return from {1} in {2} day(s) to be ready and rest before Christmas.";
+    private const string MessageForOverdue = "Overdue for {0} located {1}.";
+    
+    public string ComposeMessage(Reindeer reindeer, int numberOfDaysBeforeChristmas) 
+        => string.Format(MessageForReindeerToReturn,
+                reindeer.Name,
+                reindeer.CurrentLocation,
+                new DaysForReturn(reindeer, numberOfDaysToRest).AndDaysBeforeChristmas(numberOfDaysBeforeChristmas).Value);
 
-    public bool IsOverdue(
-        Reindeer reindeer,
-        int numberOfDaysBeforeChristmas,
-        ILogger logger)
+    public bool IsOverdue(Reindeer reindeer, int numberOfDaysBeforeChristmas, ILogger logger)
     {
-        if (DaysBeforeReturn(reindeer.NumbersOfDaysForComingBack, numberOfDaysBeforeChristmas) <= 0)
-        {
-            logger.Log($"Overdue for {reindeer.Name} located {reindeer.CurrentLocation}.");
-            return true;
+        if (new DaysForReturn(reindeer, numberOfDaysToRest)
+            .AndDaysBeforeChristmas(numberOfDaysBeforeChristmas).IsEnough) {
+            return false;
         }
-
-        return false;
+        
+        logger.Log(string.Format(MessageForOverdue, reindeer.Name, reindeer.CurrentLocation));
+        return true;
     }
-
-    private int DaysBeforeReturn(int numbersOfDaysForComingBack, int numberOfDaysBeforeChristmas) =>
-        numberOfDaysBeforeChristmas - numbersOfDaysForComingBack - numberOfDaysToRest;
 }
