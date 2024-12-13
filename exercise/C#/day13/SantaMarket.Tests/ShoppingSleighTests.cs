7bee865dd44eb4fc356d766078fa5d0af16e5335
cedric <cedric.meleard@gmail.com>
2024-12-13 20:14:58 +0100

    Mikado: Add Offer

--- exercise/C#/day13/SantaMarket.Tests/ShoppingSleighTests.cs
+++ exercise/C#/day13/SantaMarket.Tests/ShoppingSleighTests.cs
@@ -9,11 +9,12 @@ public class ShoppingSleighTests
     [Fact]
     public void HandleOffXforYers_Should_Return_ADiscounted_Price()
     {
+        // Instanciate a product with a Product Unit Each
         // Instanciate an offer with a quantity of 3 with a price of 100$
-        // Instanciate a product with a quantity of 3
         // Assert that it returns a Discount of 100$
         var product = new Product(new Faker().Commerce.ProductName(), ProductUnit.Each);
         
         
+        var offer = new Offer(SpecialOfferType.ThreeForTwo, product, new Faker().Random.Double());
     }
 }
