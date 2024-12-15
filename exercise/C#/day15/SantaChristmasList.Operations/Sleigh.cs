9731bfd16174f24fcd551ef19358c38360ae2c13
cedric <cedric.meleard@gmail.com>
2024-12-15 15:36:47 +0100

    refactor: remove unused ContainsGiftFor method

--- exercise/C#/day15/SantaChristmasList.Operations/Sleigh.cs
+++ exercise/C#/day15/SantaChristmasList.Operations/Sleigh.cs
@@ -2,7 +2,6 @@ namespace SantaChristmasList.Operations;
 
 public class Sleigh : Dictionary<Child, string>
 {
-    public bool ContainsGiftFor(Child child) => ContainsKey(child);
     public string GetGiftFor(Child child)
     {
         return !ContainsKey(child)
