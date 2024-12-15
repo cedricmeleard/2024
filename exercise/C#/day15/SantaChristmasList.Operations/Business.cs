1555e84c1d36645c12dca82c52957c490e69c998
cedric <cedric.meleard@gmail.com>
2024-12-15 16:55:18 +0100

    refactor: use Option type for safer gift handling

--- exercise/C#/day15/SantaChristmasList.Operations/Business.cs
+++ exercise/C#/day15/SantaChristmasList.Operations/Business.cs
@@ -7,19 +7,16 @@ public class Business(Factory factory, Inventory inventory, WishList wishList)
         var list = new Sleigh();
         foreach (var child in children)
         {
-            var gift = wishList.IdentifyGift(child);
-            if (gift is not null)
-            {
-                factory
-                    .FindManufacturedGift(gift)
-                    .Match(manufactured
-                            => inventory
-                                .PickUpGift(manufactured.BarCode)
-                                .Match(pickedGift => list.AddGift(child, pickedGift),
-                                    () => list.AddMisplacedGift(child)),
-                        () => list.AddNotManufactured(child)
-                    );
-            }
+            wishList.IdentifyGift(child)
+                .IfSome(
+                    gift => factory
+                        .FindManufacturedGift(gift)
+                        .Match(manufactured
+                                => inventory
+                                    .PickUpGift(manufactured.BarCode)
+                                    .Match(pickedGift => list.AddGift(child, pickedGift),
+                                        () => list.AddMisplacedGift(child)),
+                            () => list.AddNotManufactured(child)));
         }
         return list;
     }
--- exercise/C#/day15/SantaChristmasList.Operations/Dependencies.cs
+++ exercise/C#/day15/SantaChristmasList.Operations/Dependencies.cs
@@ -18,8 +18,8 @@ public class Inventory : Dictionary<string, Gift>
 
 public class WishList : Dictionary<Child, Gift>
 {
-    public Gift IdentifyGift(Child child)
+    public Option<Gift> IdentifyGift(Child child)
     {
-        return ContainsKey(child) ? this[child] : null;
+        return ContainsKey(child) ? this[child] : Option<Gift>.None;
     }
 }
