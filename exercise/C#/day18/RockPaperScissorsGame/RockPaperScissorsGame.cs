f1897f33fb0a75d024c1a5d10160039e92e94242
cedric <cedric.meleard@gmail.com>
2024-12-18 22:32:56 +0100

    refactor: extract DetermineWinningResult method
    - Extract method: VerifyPlayerWin -> DetermineWinningResult
    - Simplify logic using switch expression

--- exercise/C#/day18/RockPaperScissorsGame/RockPaperScissorsGame.cs
+++ exercise/C#/day18/RockPaperScissorsGame/RockPaperScissorsGame.cs
@@ -21,37 +21,28 @@ namespace RockPaperScissorsGame
     public static class RockPaperScissors
     {
         public static Result? Play(Choice player1, Choice player2)
-        {
-            return VerifyPlayerWin(player1, player2, Winner.Player1) 
-                   ?? VerifyPlayerWin(player2, player1, Winner.Player2);
-        }
+            => VerifyPlayerWin(player1, player2, Winner.Player1) 
+               ?? VerifyPlayerWin(player2, player1, Winner.Player2);
 
         private static Result? VerifyPlayerWin(Choice shouldWin, Choice mightLoose, Winner winner)
-        {
-            if (shouldWin == mightLoose)
-                return new Result(Winner.Draw, "same choice");
-            else if (shouldWin == Choice.Rock && mightLoose == Choice.Scissors)
-                return new Result(winner, "rock crushes scissors");
-            else if (shouldWin == Choice.Rock && mightLoose == Choice.Lizard)
-                return new Result(winner, "rock crushes lizard");
-            else if (shouldWin == Choice.Paper && mightLoose == Choice.Rock)
-                return new Result(winner, "paper covers rock");
-            else if (shouldWin == Choice.Paper && mightLoose == Choice.Spock)
-                return new Result(winner, "paper disproves spock");
-            else if (shouldWin == Choice.Scissors && mightLoose == Choice.Paper)
-                return new Result(winner, "scissors cuts paper");
-            else if (shouldWin == Choice.Scissors && mightLoose == Choice.Lizard)
-                return new Result(winner, "scissors decapitates lizard");
-            else if (shouldWin == Choice.Lizard && mightLoose == Choice.Spock)
-                return new Result(winner, "lizard poisons spock");
-            else if (shouldWin == Choice.Lizard && mightLoose == Choice.Paper)
-                return new Result(winner, "lizard eats paper");
-            else if (shouldWin == Choice.Spock && mightLoose == Choice.Rock)
-                return new Result(winner, "spock vaporizes rock");
-            else if (shouldWin == Choice.Spock && mightLoose == Choice.Scissors)
-                return new Result(winner, "spock smashes scissors");
+            => shouldWin == mightLoose 
+                ? new Result(Winner.Draw, "same choice") 
+                : DetermineWinningResult(shouldWin, mightLoose, winner);
 
-            return null;
-        }
+        private static Result? DetermineWinningResult(Choice shouldWin, Choice mightLoose, Winner winner)
+            => shouldWin switch
+            {
+                Choice.Rock when mightLoose == Choice.Scissors => new Result(winner, "rock crushes scissors"),
+                Choice.Rock when mightLoose == Choice.Lizard => new Result(winner, "rock crushes lizard"),
+                Choice.Paper when mightLoose == Choice.Rock => new Result(winner, "paper covers rock"),
+                Choice.Paper when mightLoose == Choice.Spock => new Result(winner, "paper disproves spock"),
+                Choice.Scissors when mightLoose == Choice.Paper => new Result(winner, "scissors cuts paper"),
+                Choice.Scissors when mightLoose == Choice.Lizard => new Result(winner, "scissors decapitates lizard"),
+                Choice.Lizard when mightLoose == Choice.Spock => new Result(winner, "lizard poisons spock"),
+                Choice.Lizard when mightLoose == Choice.Paper => new Result(winner, "lizard eats paper"),
+                Choice.Spock when mightLoose == Choice.Rock => new Result(winner, "spock vaporizes rock"),
+                Choice.Spock when mightLoose == Choice.Scissors => new Result(winner, "spock smashes scissors"),
+                _ => null
+            };
     }
 }
