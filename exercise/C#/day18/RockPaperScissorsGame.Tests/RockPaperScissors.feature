30e66f5add3f11142551631e1f10cb9b83af6969
cedric <cedric.meleard@gmail.com>
2024-12-18 22:13:54 +0100

    feat: add extended rules for Rock-Paper-Scissors-Lizard-Spock
    - Added rules for Player 1 victories:
      - rock crushes lizard
      - scissors decapitates lizard
      - paper disproves spock
      - lizard poisons spock
      - spock vaporizes rock
    - Added rules for Player 2 victories:
      - rock crushes lizard
      - scissors decapitates lizard
      - paper disproves spock
      - lizard poisons spock
      - spock vaporizes rock

--- exercise/C#/day18/RockPaperScissorsGame.Tests/RockPaperScissors.feature
+++ exercise/C#/day18/RockPaperScissorsGame.Tests/RockPaperScissors.feature
@@ -54,18 +54,54 @@ Feature: Rock Paper Scissors Game
         When they play
         Then the result should be Player 1 because spock smashes scissors
     
-    Scenario: Player 2 wins with Scissors over Paper
-        Given Player 1 chooses 📄
-        And Player 2 chooses ✂️
-        When they play
-        Then the result should be Player 2 because scissors cuts paper
-
-    Scenario: Player 2 wins with Rock over Scissors
-        Given Player 1 chooses ✂️
+   Scenario: Player 2 wins with Rock over Lizard
+        Given Player 1 chooses 🦎
         And Player 2 chooses 🪨
         When they play
-        Then the result should be Player 2 because rock crushes scissors
+        Then the result should be Player 2 because rock crushes lizard
 
+    Scenario: Player 2 wins with Scissors over Lizard
+        Given Player 1 chooses 🦎
+        And Player 2 chooses ✂️
+        When they play
+        Then the result should be Player 2 because scissors decapitates lizard
+
+    Scenario: Player 2 wins with Spock over Scissors
+        Given Player 1 chooses ✂️
+        And Player 2 chooses 🖖
+        When they play
+        Then the result should be Player 2 because spock smashes scissors
+
+    Scenario: Player 2 wins with Lizard over Paper
+        Given Player 1 chooses 📄
+        And Player 2 chooses 🦎
+        When they play
+        Then the result should be Player 2 because lizard eats paper
+
+    Scenario: Player 2 wins with Lizard over Spock
+        Given Player 1 chooses 🖖
+        And Player 2 chooses 🦎
+        When they play
+        Then the result should be Player 2 because lizard poisons spock
+
+    Scenario: Player 2 wins with Paper over Rock
+        Given Player 1 chooses 🪨
+        And Player 2 chooses 📄
+        When they play
+        Then the result should be Player 2 because paper covers rock
+
+    Scenario: Player 2 wins with Paper over Spock
+        Given Player 1 chooses 🖖
+        And Player 2 chooses 📄
+        When they play
+        Then the result should be Player 2 because paper disproves spock
+
+    Scenario: Player 2 wins with Spock over Rock
+        Given Player 1 chooses 🪨
+        And Player 2 chooses 🖖
+        When they play
+        Then the result should be Player 2 because spock vaporizes rock
+   
     Scenario Outline: Draw
         Given Player 1 chooses <choice>
         And Player 2 chooses <choice>
--- exercise/C#/day18/RockPaperScissorsGame/RockPaperScissorsGame.cs
+++ exercise/C#/day18/RockPaperScissorsGame/RockPaperScissorsGame.cs
@@ -26,28 +26,46 @@ namespace RockPaperScissorsGame
                 return new Result(Winner.Draw, "same choice");
             else if (player1 == Choice.Rock && player2 == Choice.Scissors)
                 return new Result(Winner.Player1, "rock crushes scissors");
-            else if (player1 == Choice.Paper && player2 == Choice.Rock)
-                return new Result(Winner.Player1, "paper covers rock");
-            else if (player1 == Choice.Scissors && player2 == Choice.Paper)
-                return new Result(Winner.Player1, "scissors cuts paper");
-            else if (player2 == Choice.Rock && player1 == Choice.Scissors)
-                return new Result(Winner.Player2, "rock crushes scissors");
-            else if (player2 == Choice.Paper && player1 == Choice.Rock)
-                return new Result(Winner.Player2, "paper covers rock");
-            else if(player2 == Choice.Scissors && player1 == Choice.Paper)
-                return new Result(Winner.Player2, "scissors cuts paper");
-            else if (player1 == Choice.Lizard && player2 == Choice.Spock)
-                return new Result(Winner.Player1, "lizard poisons spock");
-            else if (player1 == Choice.Spock && player2 == Choice.Rock)
-                return new Result(Winner.Player1, "spock vaporizes rock");
-            else if (player1 == Choice.Lizard && player2 == Choice.Paper)
-                return new Result(Winner.Player1, "lizard eats paper");
-            else if (player1 == Choice.Paper && player2 == Choice.Spock)
-                return new Result(Winner.Player1, "paper disproves spock");
             else if (player1 == Choice.Rock && player2 == Choice.Lizard)
                 return new Result(Winner.Player1, "rock crushes lizard");
+            else if (player1 == Choice.Paper && player2 == Choice.Rock)
+                return new Result(Winner.Player1, "paper covers rock");
+            else if (player1 == Choice.Paper && player2 == Choice.Spock)
+                return new Result(Winner.Player1, "paper disproves spock");
+            else if (player1 == Choice.Scissors && player2 == Choice.Paper)
+                return new Result(Winner.Player1, "scissors cuts paper");
+            else if (player1 == Choice.Scissors && player2 == Choice.Lizard)
+                return new Result(Winner.Player1, "scissors decapitates lizard");
+            else if (player1 == Choice.Lizard && player2 == Choice.Spock)
+                return new Result(Winner.Player1, "lizard poisons spock");
+            else if (player1 == Choice.Lizard && player2 == Choice.Paper)
+                return new Result(Winner.Player1, "lizard eats paper");
+            else if (player1 == Choice.Spock && player2 == Choice.Rock)
+                return new Result(Winner.Player1, "spock vaporizes rock");
             else if (player1 == Choice.Spock && player2 == Choice.Scissors)
                 return new Result(Winner.Player1, "spock smashes scissors");
+            
+            else if (player2 == Choice.Rock && player1 == Choice.Scissors)
+                return new Result(Winner.Player2, "rock crushes scissors");
+            else if (player2 == Choice.Rock && player1 == Choice.Lizard)
+                return new Result(Winner.Player2, "rock crushes lizard");
+            else if (player2 == Choice.Paper && player1 == Choice.Rock)
+                return new Result(Winner.Player2, "paper covers rock");
+            else if (player2 == Choice.Paper && player1 == Choice.Spock)
+                return new Result(Winner.Player2, "paper disproves spock");
+            else if(player2 == Choice.Scissors && player1 == Choice.Paper)
+                return new Result(Winner.Player2, "scissors cuts paper");
+            else if (player2 == Choice.Scissors && player1 == Choice.Lizard)
+                return new Result(Winner.Player2, "scissors decapitates lizard");
+            else if (player2 == Choice.Lizard && player1 == Choice.Spock)
+                return new Result(Winner.Player2, "lizard poisons spock");
+            else if (player2 == Choice.Lizard && player1 == Choice.Paper)
+                return new Result(Winner.Player2, "lizard eats paper");
+            else if (player2 == Choice.Spock && player1 == Choice.Rock)
+                return new Result(Winner.Player2, "spock vaporizes rock");
+            else if (player2 == Choice.Spock && player1 == Choice.Scissors)
+                return new Result(Winner.Player2, "spock smashes scissors");
+            
             else
                 return null;
                 
