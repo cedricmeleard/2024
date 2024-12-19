83886b741b45cb52404309b87d14baac4b22cfed
cedric <cedric.meleard@gmail.com>
2024-12-19 19:12:25 +0100

    Simplify recursive distance calculation method.
    Replaced the recursive implementation with a concise expression-bodied method. This change enhances readability and eliminates unnecessary recursive logic for better performance and maintainability.

--- exercise/C#/day19/Travel/SantaTravelCalculator.cs
+++ exercise/C#/day19/Travel/SantaTravelCalculator.cs
@@ -3,13 +3,6 @@
 public static class SantaTravelCalculator
 {
     public static long CalculateTotalDistanceRecursively(int numberOfReindeers)
-    {
-        if (numberOfReindeers == 1) return 1;
-        checked
-        {
-            return (long)(Math
-                .Pow(2, numberOfReindeers) - 1);
-        }
-    }
+        => (long)(Math.Pow(2, numberOfReindeers) - 1);
 }
 }
