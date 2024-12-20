b76e41dd16c8df30cd3c33484fb7684c3d1651cc
cedric <cedric.meleard@gmail.com>
2024-12-20 07:50:03 +0100

    Add benchmark project and extend reindeer distance calculations
    Introduce a new Benchmark project to measure performance of reindeer distance calculations. Added multiple calculation methods (Math.Pow, recursive, loop, and switch-based) to the main calculator. Updated test cases and constraints to limit calculations to 53 reindeer.

--- exercise/C#/day19/Travel.Tests/CalculatorTests.cs
+++ exercise/C#/day19/Travel.Tests/CalculatorTests.cs
@@ -1,3 +1,4 @@
+using System.Text;
 using FluentAssertions;
 using FsCheck;
 using FsCheck.Xunit;
@@ -12,12 +13,12 @@ namespace Travel.Tests
         [InlineData(1, 1)]
         [InlineData(2, 3)]
         [InlineData(5, 31)]
-        [InlineData(10, 1023)]
-        [InlineData(20, 1048575)]
-        [InlineData(30, 1073741823)]
-        [InlineData(32, 4294967295)]
-        [InlineData(50, 1125899906842623)]
-        [InlineData(63, 9223372036854775808)]
+        [InlineData(10, 1_023)]
+        [InlineData(20, 1_048_575)]
+        [InlineData(30, 1_073_741_823)]
+        [InlineData(32, 4_294_967_295)]
+        [InlineData(50, 1_125_899_906_842_623)]
+        [InlineData(53, 9_007_199_254_740_991)]
         public void Should_Calculate_The_DistanceFor(int numberOfReindeers, ulong expectedDistance)
             => CalculateTotalDistance(numberOfReindeers)
                 .Should()
@@ -27,7 +28,7 @@ namespace Travel.Tests
         public Property Should_Calculate_The_DistanceFor_Any_Number_Of_Reindeers()
         {
             return Prop.ForAll(
-                Gen.Choose(1, 63).ToArbitrary(),
+                Gen.Choose(1, 53).ToArbitrary(),
                 numberOfReindeers
                     => CalculateTotalDistance(numberOfReindeers)
                         .Should()
@@ -46,16 +47,29 @@ namespace Travel.Tests
         }
         
         [Property]
-        public Property Should_Not_Calculate_The_DistanceFor_Any_Number_Of_Reindeers_GreaterThan63()
+        public Property Should_Not_Calculate_The_DistanceFor_Any_Number_Of_Reindeers_GreaterThan53()
             => Prop.ForAll(
-                Gen.Choose(64, int.MaxValue).ToArbitrary(),
+                Gen.Choose(54, int.MaxValue).ToArbitrary(),
                 numberOfReindeers =>
                 {
                     var act = () => CalculateTotalDistance(numberOfReindeers);
                     act
                         .Should()
                         .Throw<ArgumentException>()
-                        .WithMessage("The number of reindeers must be less than or equal to 63 (Parameter 'numberOfReindeers')");
+                        .WithMessage("The number of reindeers must be less than or equal to 53 (Parameter 'numberOfReindeers')");
                 });
+        
+        [Fact]
+        public async Task Verify_Calculate_The_DistanceFor_Reindeers_Recursively()
+        {
+            var sb = new StringBuilder();
+
+            for (var numberOfReindeers = 1; numberOfReindeers <= 53; numberOfReindeers++)
+            {
+                sb.AppendLine($"{numberOfReindeers} => {CalculateTotalDistance(numberOfReindeers)},");
+            }
+            
+            await Verify(sb.ToString());
+        }
     }
 }
--- exercise/C#/day19/Travel/SantaTravelCalculator.cs
+++ exercise/C#/day19/Travel/SantaTravelCalculator.cs
@@ -4,17 +4,46 @@ namespace Travel
 {
 public static class SantaTravelCalculator
 {
-    private const string TheNumberOfReindeersMustBeLessThanOrEqualTo = "The number of reindeers must be less than or equal to 63";
+    private const string TheNumberOfReindeersMustBeLessThanOrEqualTo = "The number of reindeers must be less than or equal to 53";
     private const string TheNumberOfReindeersMustBeGreaterThan = "The number of reindeers must be greater than 0";
-    private const int MaxReindeer = 63;
+    private const int MaxReindeer = 53;
 
     public static ulong CalculateTotalDistance(int numberOfReindeers)
     {
         EnsureValidReindeerNumber(numberOfReindeers);
 
+        return CalculateDistanceForReindeersMathPow(numberOfReindeers);
+    }
+    
+    public static ulong CalculateDistanceForReindeersMathPow(int numberOfReindeers)
+    {
+        if (numberOfReindeers == 1) return 1;
         return (ulong)(Math.Pow(2, numberOfReindeers) - 1);
     }
-
+    
+    public static ulong CalculateTotalDistanceRecursively(int numberOfReindeers)
+    {
+        if (numberOfReindeers == 1) return 1;
+        checked
+        {
+            return 2 * CalculateTotalDistanceRecursively(numberOfReindeers - 1) + 1;
+        }
+    }
+    
+    public static ulong CalculateTotalDistanceLoop(int numberOfReindeers)
+    {
+        if (numberOfReindeers == 1) return 1;
+        checked
+        {
+            ulong result = 0;
+            for (int i = numberOfReindeers; i > 0; i--)
+            {
+                result = 2 * result + 1;
+            }
+            return result;
+        }
+    }
+    
     private static void EnsureValidReindeerNumber(int numberOfReindeers)
     {
         Guard.Against.NegativeOrZero(
