0af1842ab6140c2ebb11d7f7439962629d72448b
cedric <cedric.meleard@gmail.com>
2024-12-20 22:22:49 +0100

    Add Verify over GetReindeer Test
    Introduced contract testing with a verified response file for `ShouldGetReindeer`.
    Improved code organization by grouping failure scenarios into a dedicated class.

--- exercise/C#/day20/Reindeer.Web.Tests/ReindeerContractTests.cs
+++ exercise/C#/day20/Reindeer.Web.Tests/ReindeerContractTests.cs
@@ -19,25 +19,7 @@ namespace Reindeer.Web.Tests
         public async Task ShouldGetReindeer()
         {
             var response = await _client.GetAsync("reindeer/40F9D24D-D3E0-4596-ADC5-B4936FF84B19");
-            response.StatusCode.Should().Be(HttpStatusCode.OK);
-        }
-        
-        [Fact]
-        public async Task NotFoundForNotExistingReindeer()
-        {
-            var nonExistingReindeer = Guid.NewGuid().ToString();
-            var response = await _client.GetAsync($"reindeer/{nonExistingReindeer}");
-
-            response.StatusCode.Should().Be(HttpStatusCode.NotFound);
-        }
-        
-        [Fact]
-        public async Task ConflictWhenTryingToCreateExistingOne()
-        {
-            var request = new ReindeerToCreateRequest("Petar", ReindeerColor.Purple);
-            var response = await _client.PostAsync("reindeer", JsonContent.Create(request));
-
-            response.StatusCode.Should().Be(HttpStatusCode.Conflict);
+            await Verify(response);
         }
         
         [Fact]
@@ -48,14 +30,44 @@ namespace Reindeer.Web.Tests
 
             await Verify(response);
         }
-        
-        [Fact]
-        public async Task ErrorWhenCreatingReindeerWithoutDatas()
-        {
-            var response = await _client.PostAsync("reindeer", null);
 
-            response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+        public class ReindeerFailures
+        {
+            private readonly HttpClient _client;
+
+            public ReindeerFailures()
+            {
+                var webApplication = new ReindeerWebApplicationFactory();
+                _client = webApplication.CreateClient();
+            }
+            
+            [Fact]
+            public async Task ErrorWhenCreatingReindeerWithoutDatas()
+            {
+                var response = await _client.PostAsync("reindeer", null);
+                response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+            }
+            
+            [Fact]
+            public async Task NotFoundForNotExistingReindeer()
+            {
+                var nonExistingReindeer = Guid.NewGuid().ToString();
+                var response = await _client.GetAsync($"reindeer/{nonExistingReindeer}");
+
+                response.StatusCode.Should().Be(HttpStatusCode.NotFound);
+            }
+        
+            [Fact]
+            public async Task ConflictWhenTryingToCreateExistingOne()
+            {
+                var request = new ReindeerToCreateRequest("Petar", ReindeerColor.Purple);
+                var response = await _client.PostAsync("reindeer", JsonContent.Create(request));
+
+                response.StatusCode.Should().Be(HttpStatusCode.Conflict);
+            }
         }
+        
+        
         
     }
 }
