6123aafc000f254a77a66955f6ccfa70ce0dc0b0
cedric <cedric.meleard@gmail.com>
2024-12-22 11:07:15 +0100

    feat: refactor game result handling using LanguageExt
    - Added `LanguageExt.Core` package
    - Updated `Play` method to utilize `Either` and `Match`
    - Refactored `VerifyPlayerWin` to return `Either<AGameWin, AGameDraw>`
    - Introduced `AGameWin` and `AGameDraw` records

--- exercise/C#/day18/RockPaperScissorsGame/RockPaperScissorsGame.cs
+++ exercise/C#/day18/RockPaperScissorsGame/RockPaperScissorsGame.cs
@@ -1,3 +1,5 @@
+using LanguageExt;
+
 namespace RockPaperScissorsGame
 {
     public enum Choice
@@ -20,29 +22,31 @@ namespace RockPaperScissorsGame
 
     public static class RockPaperScissors
     {
-        public static Result? Play(Choice player1, Choice player2)
-            => VerifyPlayerWin(player1, player2, Winner.Player1) 
-               ?? VerifyPlayerWin(player2, player1, Winner.Player2);
+        public static Result Play(Choice player1, Choice player2)
+            => VerifyPlayerWin(player1, player2, Winner.Player1)
+                .Bind(_ => VerifyPlayerWin(player2, player1, Winner.Player2))
+                .Match(
+                    draw => new Result(Winner.Draw, "same choice"),
+                    win => new Result(win.Winner, win.Reason)
+                );
 
-        private static Result? VerifyPlayerWin(Choice shouldWin, Choice mightLoose, Winner winner)
-            => shouldWin == mightLoose 
-                ? new Result(Winner.Draw, "same choice") 
-                : DetermineWinningResult(shouldWin, mightLoose, winner);
-
-        private static Result? DetermineWinningResult(Choice shouldWin, Choice mightLoose, Winner winner)
+        private static Either<AGameWin, AGameDraw> VerifyPlayerWin(Choice shouldWin, Choice mightLoose, Winner winner)
             => shouldWin switch
             {
-                Choice.Rock when mightLoose == Choice.Scissors => new Result(winner, "rock crushes scissors"),
-                Choice.Rock when mightLoose == Choice.Lizard => new Result(winner, "rock crushes lizard"),
-                Choice.Paper when mightLoose == Choice.Rock => new Result(winner, "paper covers rock"),
-                Choice.Paper when mightLoose == Choice.Spock => new Result(winner, "paper disproves spock"),
-                Choice.Scissors when mightLoose == Choice.Paper => new Result(winner, "scissors cuts paper"),
-                Choice.Scissors when mightLoose == Choice.Lizard => new Result(winner, "scissors decapitates lizard"),
-                Choice.Lizard when mightLoose == Choice.Spock => new Result(winner, "lizard poisons spock"),
-                Choice.Lizard when mightLoose == Choice.Paper => new Result(winner, "lizard eats paper"),
-                Choice.Spock when mightLoose == Choice.Rock => new Result(winner, "spock vaporizes rock"),
-                Choice.Spock when mightLoose == Choice.Scissors => new Result(winner, "spock smashes scissors"),
-                _ => null
+                Choice.Rock when mightLoose == Choice.Scissors => new AGameWin(winner, "rock crushes scissors"),
+                Choice.Rock when mightLoose == Choice.Lizard => new AGameWin(winner, "rock crushes lizard"),
+                Choice.Paper when mightLoose == Choice.Rock => new AGameWin(winner, "paper covers rock"),
+                Choice.Paper when mightLoose == Choice.Spock => new AGameWin(winner, "paper disproves spock"),
+                Choice.Scissors when mightLoose == Choice.Paper => new AGameWin(winner, "scissors cuts paper"),
+                Choice.Scissors when mightLoose == Choice.Lizard => new AGameWin(winner, "scissors decapitates lizard"),
+                Choice.Lizard when mightLoose == Choice.Spock => new AGameWin(winner, "lizard poisons spock"),
+                Choice.Lizard when mightLoose == Choice.Paper => new AGameWin(winner, "lizard eats paper"),
+                Choice.Spock when mightLoose == Choice.Rock => new AGameWin(winner, "spock vaporizes rock"),
+                Choice.Spock when mightLoose == Choice.Scissors => new AGameWin(winner, "spock smashes scissors"),
+                _ => new AGameDraw()
             };
     }
+
+    public record AGameDraw;
+    public record AGameWin(Winner Winner, string Reason);
 }
