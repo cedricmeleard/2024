935b7ab7c456a537709340882b5b991dce159b29
cedric <cedric.meleard@gmail.com>
2024-12-22 08:59:41 +0100

    `refactor: simplify step processing in NewTourCalculator`
    - Extracted `logSteps()` as an extension function for `List<Step>`.
    - Moved delivery time calculation to `appendDeliveryTime()` for better separation of concerns.
    - Changed `calculateSteps` to a chain of operations for improved readability.
    - Renamed and reordered methods to align with logical flow:
      - Moved `fLine` above `toSortedSteps` for better clarity.
      - Created a new `appendDeliveryTime` from delivery time formatting logic.

--- exercise/kotlin/day21/src/main/kotlin/tour/NewTourCalculator.kt
+++ exercise/kotlin/day21/src/main/kotlin/tour/NewTourCalculator.kt
@@ -13,22 +13,24 @@ class NewTourCalculator(private var steps: List<Step>) {
         calculateSteps(steps.toSortedSteps()).right()
     } else "No locations !!!".left()
 
-    private fun calculateSteps(steps: List<Step>): String {
+    private fun calculateSteps(steps: List<Step>): String = steps
+        .logSteps()
+        .appendDeliveryTime(steps)
+        .toString()
+
+    private fun fLine(step: Step): String = step.let { "${it.time} : ${it.label} | ${it.deliveryTime} sec" }
+    private fun List<Step>.toSortedSteps(): List<Step> = this.filter { true }.sortedBy { it.time }
+    private fun List<Step>.logSteps(): StringBuilder {
         val result = StringBuilder()
-
-        steps.forEach { step -> result.appendLine(fLine(step)) }
-
-        val str: String = formatDurationToHHMMSS(steps.sumOf { it.deliveryTime }.toLong())
-        result.appendLine("Delivery time | $str")
-
-        return result.toString()
+        this.forEach { step -> result.appendLine(fLine(step)) }
+        return result
     }
+    
+    private fun StringBuilder.appendDeliveryTime(steps: List<Step>) =
+        this.appendLine("Delivery time | ${formatDurationToHHMMSS(steps.sumOf { it.deliveryTime }.toLong())}")
 
     private fun formatDurationToHHMMSS(deliveryTime: Long): String {
         val duration = Duration.ofSeconds(deliveryTime)
         return "${duration.toHours()}%02d:${duration.toMinutesPart()}%02d:${duration.toSecondsPart()}%02d"
     }
-
-    private fun fLine(step: Step): String = step.let { "${it.time} : ${it.label} | ${it.deliveryTime} sec" }
-    private fun List<Step>.toSortedSteps(): List<Step> = this.filter { true }.sortedBy { it.time }
 }
