67bb7a18f213203c438446c7716d87654edef8d3
cedric <cedric.meleard@gmail.com>
2024-12-22 11:22:45 +0100

    refactor: rename RockPaperScissors to RockPaperScissorsLizardSpock
    - Rename:
      - Class: RockPaperScissors -> RockPaperScissorsLizardSpock
    - Update namespace to file-scoped syntax

--- exercise/C#/day18/RockPaperScissorsGame.Tests/RockPaperScissorsSteps.cs
+++ exercise/C#/day18/RockPaperScissorsGame.Tests/RockPaperScissorsSteps.cs
@@ -1,53 +1,52 @@
 using FluentAssertions;
 using TechTalk.SpecFlow;
 
-namespace RockPaperScissorsGame.Tests
+namespace RockPaperScissorsGame.Tests;
+
+[Binding]
+public class RockPaperScissorsSteps
 {
-    [Binding]
-    public class RockPaperScissorsSteps
+    private Result? _result;
+    private Choice _player1Choice;
+    private Choice _player2Choice;
+
+    [Given(@"Player (\d+) chooses (.*)")]
+    public void GivenPlayerChooses(int player, string choice)
     {
-        private Result? _result;
-        private Choice _player1Choice;
-        private Choice _player2Choice;
+        var parsedChoice = ParseChoice(choice);
 
-        [Given(@"Player (\d+) chooses (.*)")]
-        public void GivenPlayerChooses(int player, string choice)
-        {
-            var parsedChoice = ParseChoice(choice);
-
-            if (player == 1) _player1Choice = parsedChoice;
-            else _player2Choice = parsedChoice;
-        }
-
-        [When(@"they play")]
-        public void WhenTheyPlay() => _result = RockPaperScissors.Play(_player1Choice, _player2Choice);
-
-        [Then(@"the result should be (.*) because (.*)")]
-        public void ThenTheResultShouldBeBecause(string expectedWinner, string expectedReason)
-        {
-            _result!.Winner.Should().Be(
-                ParseWinner(expectedWinner)
-            );
-            _result.Reason.Should().Be(expectedReason);
-        }
-
-        private static Choice ParseChoice(string choice)
-            => choice switch
-            {
-                "🪨" => Choice.Rock,
-                "📄" => Choice.Paper,
-                "✂️" => Choice.Scissors,
-                "\ud83d\udd96" => Choice.Spock,
-                "\ud83e\udd8e" => Choice.Lizard,
-                _ => throw new ArgumentException("Invalid choice")
-            };
-        private static Winner ParseWinner(string winner)
-            => winner switch
-            {
-                "Player 1" => Winner.Player1,
-                "Player 2" => Winner.Player2,
-                "Draw" => Winner.Draw,
-                _ => throw new ArgumentException("Invalid winner")
-            };
+        if (player == 1) _player1Choice = parsedChoice;
+        else _player2Choice = parsedChoice;
     }
+
+    [When(@"they play")]
+    public void WhenTheyPlay() => _result = RockPaperScissorsLizardSpock.Play(_player1Choice, _player2Choice);
+
+    [Then(@"the result should be (.*) because (.*)")]
+    public void ThenTheResultShouldBeBecause(string expectedWinner, string expectedReason)
+    {
+        _result!.Winner.Should().Be(
+            ParseWinner(expectedWinner)
+        );
+        _result.Reason.Should().Be(expectedReason);
+    }
+
+    private static Choice ParseChoice(string choice)
+        => choice switch
+        {
+            "🪨" => Choice.Rock,
+            "📄" => Choice.Paper,
+            "✂️" => Choice.Scissors,
+            "\ud83d\udd96" => Choice.Spock,
+            "\ud83e\udd8e" => Choice.Lizard,
+            _ => throw new ArgumentException("Invalid choice")
+        };
+    private static Winner ParseWinner(string winner)
+        => winner switch
+        {
+            "Player 1" => Winner.Player1,
+            "Player 2" => Winner.Player2,
+            "Draw" => Winner.Draw,
+            _ => throw new ArgumentException("Invalid winner")
+        };
 }
--- exercise/C#/day18/RockPaperScissorsGame/RockPaperScissorsGame.cs
+++ exercise/C#/day18/RockPaperScissorsGame/RockPaperScissorsGame.cs
@@ -1,70 +1,69 @@
 using LanguageExt;
 
-namespace RockPaperScissorsGame
+namespace RockPaperScissorsGame;
+
+public enum Choice
 {
-    public enum Choice
-    {
-        Rock,
-        Paper,
-        Scissors,
-        Spock,
-        Lizard
-    }
-
-    public enum Winner
-    {
-        Player1,
-        Player2,
-        Draw
-    }
-
-    public record Result(Winner Winner, string Reason);
-
-    public static class RockPaperScissors
-    {
-        public static Result Play(Choice player1, Choice player2)
-            => VerifyPlayerWin(player1, player2, Winner.Player1)
-                .Bind(_ => VerifyPlayerWin(player2, player1, Winner.Player2))
-                .Match(
-                    _ => throw new IncertainResultException(),
-                    win => new Result(win.Winner, win.Reason)
-                );
-
-        private static Either<IEndGame, AnIncertainResultYet> VerifyPlayerWin(Choice shouldWin, Choice mightLoose, Winner winner)
-        {
-            if (shouldWin == mightLoose) return new ADraw();
-            
-            return shouldWin switch
-            {
-                Choice.Rock when mightLoose == Choice.Scissors => new AWin(winner, "rock crushes scissors"),
-                Choice.Rock when mightLoose == Choice.Lizard => new AWin(winner, "rock crushes lizard"),
-                Choice.Paper when mightLoose == Choice.Rock => new AWin(winner, "paper covers rock"),
-                Choice.Paper when mightLoose == Choice.Spock => new AWin(winner, "paper disproves spock"),
-                Choice.Scissors when mightLoose == Choice.Paper => new AWin(winner, "scissors cuts paper"),
-                Choice.Scissors when mightLoose == Choice.Lizard => new AWin(winner, "scissors decapitates lizard"),
-                Choice.Lizard when mightLoose == Choice.Spock => new AWin(winner, "lizard poisons spock"),
-                Choice.Lizard when mightLoose == Choice.Paper => new AWin(winner, "lizard eats paper"),
-                Choice.Spock when mightLoose == Choice.Rock => new AWin(winner, "spock vaporizes rock"),
-                Choice.Spock when mightLoose == Choice.Scissors => new AWin(winner, "spock smashes scissors"),
-                _ => new AnIncertainResultYet()
-            };
-        }
-    }
-    
-    public record AnIncertainResultYet;
-
-    public interface IEndGame
-    {
-        public Winner Winner { get; }
-        public string Reason { get; }
-    }
-    public record AWin(Winner Winner, string Reason) : IEndGame;
-
-    public record ADraw : IEndGame
-    {
-        public Winner Winner => Winner.Draw;
-        public string Reason => "same choice";
-    };
-    
-    public class IncertainResultException() : Exception("Can't determine a Result");
+    Rock,
+    Paper,
+    Scissors,
+    Spock,
+    Lizard
 }
+
+public enum Winner
+{
+    Player1,
+    Player2,
+    Draw
+}
+
+public record Result(Winner Winner, string Reason);
+
+public static class RockPaperScissorsLizardSpock
+{
+    public static Result Play(Choice player1, Choice player2)
+        => VerifyPlayerWin(player1, player2, Winner.Player1)
+            .Bind(_ => VerifyPlayerWin(player2, player1, Winner.Player2))
+            .Match(
+                _ => throw new IncertainResultException(),
+                win => new Result(win.Winner, win.Reason)
+            );
+
+    private static Either<IEndGame, AnIncertainResultYet> VerifyPlayerWin(Choice shouldWin, Choice mightLoose, Winner winner)
+    {
+        if (shouldWin == mightLoose) return new ADraw();
+            
+        return shouldWin switch
+        {
+            Choice.Rock when mightLoose == Choice.Scissors => new AWin(winner, "rock crushes scissors"),
+            Choice.Rock when mightLoose == Choice.Lizard => new AWin(winner, "rock crushes lizard"),
+            Choice.Paper when mightLoose == Choice.Rock => new AWin(winner, "paper covers rock"),
+            Choice.Paper when mightLoose == Choice.Spock => new AWin(winner, "paper disproves spock"),
+            Choice.Scissors when mightLoose == Choice.Paper => new AWin(winner, "scissors cuts paper"),
+            Choice.Scissors when mightLoose == Choice.Lizard => new AWin(winner, "scissors decapitates lizard"),
+            Choice.Lizard when mightLoose == Choice.Spock => new AWin(winner, "lizard poisons spock"),
+            Choice.Lizard when mightLoose == Choice.Paper => new AWin(winner, "lizard eats paper"),
+            Choice.Spock when mightLoose == Choice.Rock => new AWin(winner, "spock vaporizes rock"),
+            Choice.Spock when mightLoose == Choice.Scissors => new AWin(winner, "spock smashes scissors"),
+            _ => new AnIncertainResultYet()
+        };
+    }
+}
+    
+public record AnIncertainResultYet;
+
+public interface IEndGame
+{
+    public Winner Winner { get; }
+    public string Reason { get; }
+}
+public record AWin(Winner Winner, string Reason) : IEndGame;
+
+public record ADraw : IEndGame
+{
+    public Winner Winner => Winner.Draw;
+    public string Reason => "same choice";
+};
+    
+public class IncertainResultException() : Exception("Can't determine a Result");
